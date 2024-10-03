fe311bddffc35b7db1f4dfabc789608528f9b91b
Pierre Barjon <barjon.pierre@gmail.com>
2024-10-03 11:49:23 +0200

    Update build

--- build/Build_NuGet.bat
+++ build/Build_NuGet.bat
@@ -1,7 +1,17 @@
 @ECHO off
 
+SETLOCAL
+
+IF "%~1" == "--help" (
+	GOTO :help
+)
+
+IF "%~1" == "-h" (
+	GOTO :help
+)
+
 SET BUILD_DIR=%~dp0
-SET SRC_DIR=%~dp0..\OpenSilver.SL4PopupMenu
+SET SRC_DIR=%~dp0..\
 SET CFG=Release
 
 REM Define the escape character for colored text
@@ -23,9 +33,17 @@ IF "%~1" == "" (
 ECHO. 
 ECHO %ESC%[95mBuilding %ESC%[0m%CFG% %ESC%[95mconfiguration%ESC%[0m
 ECHO. 
-msbuild %SRC_DIR%\SL4PopupMenuDemo.Opensilver.sln -p:Configuration=%CFG% -clp:ErrorsOnly -restore
+msbuild %SRC_DIR%\SL4PopupMenuDemo.Opensilver.sln -p:Configuration=%CFG%;OpenSilverVersion=%OpenSilverPkgVersion% -verbosity:minimal -restore
 
 ECHO. 
 ECHO %ESC%[95mPacking %ESC%[0mOpenSilver.SL4PopupMenu %ESC%[95mNuGet package%ESC%[0m
 ECHO. 
 %BUILD_DIR%\nuget.exe pack %BUILD_DIR%\nuspec\OpenSilver.SL4PopupMenu.nuspec -OutputDirectory "%BUILD_DIR%\output" -Properties "PackageVersion=%PackageVersion%;OpenSilverDependencyVersion=%OpenSilverPkgVersion%;Configuration=%CFG%"
+
+EXIT /b
+
+:help
+ECHO [1] OpenSilver.SL4PopupMenu NuGet package Version
+ECHO [2] OpenSilver Version
+
+ENDLOCAL
